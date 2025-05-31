438ce26e4423c4100bc7e6ca798ddd060580ae22
limaarthur <artglima.dev@gmail.com>
2025-05-30 21:20:13 -0300

    feat: create address entity

---
+++ backend/src/main/java/com/ignite/hairhub/entity/Address.java
@@ -0,0 +1,87 @@
+package com.ignite.hairhub.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String street;
+
+    private String city;
+
+    private String uf;
+
+    private String zipCode;
+
+    public Address() {
+    }
+
+    public Address(UUID id, String zipCode, String uf, String city, String street) {
+        this.id = id;
+        this.zipCode = zipCode;
+        this.uf = uf;
+        this.city = city;
+        this.street = street;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(id, address.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
