cd39e5f66954f86703e4eff3c09ad721b7189480
limaarthur <artglima.dev@gmail.com>
2025-06-07 18:44:50 -0300

    feat: address seed creation

---
+++ backend/src/main/resources/db/migration/V2__create-seed-address.sql
@@ -0,0 +1,3 @@
+INSERT INTO address (id, street, city, uf, zip_code) VALUES (UUID(), 'Rua A', 'São Paulo', 'SP', '01000-000');
+INSERT INTO address (id, street, city, uf, zip_code) VALUES (UUID(), 'Rua B', 'Rio de Janeiro', 'RJ', '20000-000');
+INSERT INTO address (id, street, city, uf, zip_code) VALUES (UUID(), 'Rua C', 'Curitiba', 'PR', '80000-000');
